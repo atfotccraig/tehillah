e757459c30a0b1f96c44582b32ea525e7e17a940
Christopher Pitt <cgpitt@gmail.com>
2018-12-09 11:11:26 +0200

    Add On My Knees

--- source/tracks/because-of-the-lamb/on-my-knees.js
+++ source/tracks/because-of-the-lamb/on-my-knees.js
@@ -2,9 +2,56 @@ import React from "react"
 import { Line, Repeat, Track, Verse, Yahweh } from "../../components"
 
 const OnMyKnees = props => (
-    <Track cues={["00:05"]} music="because-of-the-lamb-on-my-knees" {...props}>
+    <Track
+        cues={[
+            "00:13",
+            "00:37",
+            "01:03",
+            "01:29",
+            "02:18",
+            "03:03",
+            "03:28",
+            "04:13",
+        ]}
+        music="because-of-the-lamb-on-my-knees"
+        {...props}
+    >
         <Verse>
-            <Line>words</Line>
+            <Line>You never told me</Line>
+            <Line>that this life would be easy</Line>
+            <Line>You only promised that</Line>
+            <Line>you would never ever leave me</Line>
+        </Verse>
+        <Verse>
+            <Line>So I come to you now</Line>
+            <Line>seeking grace to hold on</Line>
+            <Line>Cause you’re the one</Line>
+            <Line>who makes me strong</Line>
+            <Line>You’re the One</Line>
+            <Line>who makes me strong</Line>
+        </Verse>
+        <Verse repeat name="one">
+            <Line>On my knees engaged in battle</Line>
+            <Line>On my knees I overcome</Line>
+            <Line>On my knees I won’t surrender</Line>
+            <Line>On my knees the day is won</Line>
+        </Verse>
+        <Verse repeat name="two">
+            <Line>On my knees praying daily</Line>
+            <Line>On my knees seeking You</Line>
+            <Line>On my knees being strengthened</Line>
+            <Line>On my knees I’m made new</Line>
+        </Verse>
+        <Verse repeat>
+            <Line>I’m pressed down but I’m not broken</Line>
+            <Line>Persecuted but not abandoned</Line>
+            <Line>Struck down but not destroyed</Line>
+            <Line>In Your presence I’m liberated</Line>
+        </Verse>
+        <Repeat name="one" />
+        <Repeat name="two" />
+        <Verse>
+            <Line>I’m made new</Line>
         </Verse>
     </Track>
 )
