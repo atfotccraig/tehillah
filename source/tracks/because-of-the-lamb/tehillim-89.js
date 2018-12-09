cf5b17ce57a3832fe47db237dfd982fe4e88cf79
Christopher Pitt <cgpitt@gmail.com>
2018-12-09 20:48:36 +0200

    Add Tehillim 89

--- source/tracks/because-of-the-lamb/tehillim-89.js
+++ source/tracks/because-of-the-lamb/tehillim-89.js
@@ -2,9 +2,49 @@ import React from "react"
 import { Line, Repeat, Track, Verse, Yahweh } from "../../components"
 
 const Tehillim89 = props => (
-    <Track cues={["00:05"]} music="because-of-the-lamb-tehillim-89" {...props}>
+    <Track
+        cues={[
+            "00:11",
+            "00:19",
+            "00:27",
+            "00:34",
+            "00:43",
+            "01:31",
+            "01:39",
+            "01:47",
+            "02:35",
+        ]}
+        music="because-of-the-lamb-tehillim-89"
+        {...props}
+    >
+        <Verse name="one">
+            <Line>
+                I sing of the kindness of <Yahweh />
+            </Line>
+            <Line>Forever, forever I sing</Line>
+        </Verse>
+        <Verse name="two">
+            <Line>With my mouth I make known</Line>
+            <Line>all your wonders</Line>
+            <Line>Forever, forever I sing</Line>
+        </Verse>
+        <Repeat name="one" />
+        <Repeat name="two" />
+        <Verse repeat name="three">
+            <Line>
+                I sing to you <Yahweh />
+            </Line>
+            <Line>You are my Elohim</Line>
+            <Line>Your word is planted in my heart</Line>
+            <Line>and makes me sing praises to you</Line>
+            <Line>praises to you, my king</Line>
+        </Verse>
+        <Repeat name="one" />
+        <Repeat name="two" />
+        <Repeat name="three" />
         <Verse>
-            <Line>words</Line>
+            <Line>praises to you</Line>
+            <Line>praises to you, my king</Line>
         </Verse>
     </Track>
 )
