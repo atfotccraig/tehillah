7d15c1a32c2a1a9cf19ef2a688e9646e0acc089b
Christopher Pitt <cgpitt@gmail.com>
2019-01-04 17:29:09 +0200

    Add two new songs
    - Song Of Deliverance
    - Your Presence

--- source/tracks/new-songs/song-of-deliverance.js
+++ source/tracks/new-songs/song-of-deliverance.js
@@ -1,20 +1,20 @@
 import React from "react"
-
-import {
-    Line,
-    Repeat,
-    Title,
-    Track,
-    Verse,
-    Yahweh,
-    Yahshua,
-} from "app/components"
-
+import { Line, Repeat, Title, Track, Verse } from "app/components"
 import Labels from "app/labels"
 
 const SongOfDeliverance = props => (
     <Track
-        cues={["00:00", "00:05"]}
+        cues={[
+            "00:00",
+            "00:18",
+            "00:39",
+            "01:00",
+            "01:20",
+            "01:41",
+            "02:02",
+            "02:22",
+            "04:04",
+        ]}
         music="new_songs_song_of_deliverance"
         {...props}
     >
@@ -22,7 +22,42 @@ const SongOfDeliverance = props => (
             <Labels.NewSongs.SongOfDeliverance />
         </Title>
         <Verse>
-            <Line>words</Line>
+            <Line>Set me as a seal upon your heart</Line>
+            <Line>Look to me as the apple of your eye</Line>
+            <Line>Never, ever let me go</Line>
+        </Verse>
+        <Verse>
+            <Line>Speak to the heavens</Line>
+            <Line>and they answer the earth</Line>
+            <Line>And make Your creatures</Line>
+            <Line>lie down in peace</Line>
+            <Line>Speak Your life into their heart</Line>
+        </Verse>
+        <Verse name="one">
+            <Line>You are my hiding place</Line>
+            <Line>You surround me with</Line>
+            <Line>songs of deliverance</Line>
+        </Verse>
+        <Verse>
+            <Line>A love everlasting do I have for you</Line>
+            <Line>Betrothed and favoured as I promised to</Line>
+            <Line>Never will I let you go</Line>
+        </Verse>
+        <Verse>
+            <Line>The rivers clap their hands</Line>
+            <Line>and the mountains sing for joy</Line>
+            <Line>The heavens rejoice</Line>
+            <Line>and the earth is glad</Line>
+            <Line>Everything is in my hand</Line>
+        </Verse>
+        <Repeat name="one" />
+        <Verse repeat repeatText="x5">
+            <Line>Rise up my love, my beautiful one</Line>
+            <Line>The winter is past, the rain, it has gone</Line>
+            <Line>The time of singing has come</Line>
+        </Verse>
+        <Verse repeat>
+            <Line>Rise, beloved, the time has come</Line>
         </Verse>
     </Track>
 )
--- source/tracks/new-songs/your-presence.js
+++ source/tracks/new-songs/your-presence.js
@@ -1,25 +1,42 @@
 import React from "react"
-
-import {
-    Line,
-    Repeat,
-    Title,
-    Track,
-    Verse,
-    Yahweh,
-    Yahshua,
-} from "app/components"
-
+import { Line, Repeat, Title, Track, Verse } from "app/components"
 import Labels from "app/labels"
 
 const YourPresence = props => (
-    <Track cues={["00:00", "00:05"]} music="new_songs_your_presence" {...props}>
+    <Track
+        cues={["00:00", "00:10", "00:33", "00:59", "01:23", "01:46"]}
+        music="new_songs_your_presence"
+        {...props}
+    >
         <Title>
             <Labels.NewSongs.YourPresence />
         </Title>
-        <Verse>
-            <Line>words</Line>
+        <Verse name="one">
+            <Line>I lift my hands, an offering</Line>
+            <Line>to honour and esteem my king</Line>
+            <Line>I raise my voice, to you I sing</Line>
+            <Line>an honest prayer of thanksgiving</Line>
         </Verse>
+        <Verse>
+            <Line>For the day I called you answered me</Line>
+            <Line>There is nowhere else I’d rather be</Line>
+            <Line>You will never change</Line>
+            <Line>and you will remain</Line>
+            <Line>Some may fall away, but I want to stay</Line>
+        </Verse>
+        <Verse>
+            <Line>In your presence is life’s essence</Line>
+            <Line>there is no-one else worth standing for</Line>
+            <Line>In your presence is life’s essence</Line>
+            <Line>You can fill my life with joy and peace</Line>
+        </Verse>
+        <Verse>
+            <Line>In your presence is life’s essence</Line>
+            <Line>there is nothing else worth bowing to</Line>
+            <Line>In your presence is life’s essence</Line>
+            <Line>it is why I live, I will praise you</Line>
+        </Verse>
+        <Repeat name="one" />
     </Track>
 )
 
