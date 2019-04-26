23c931a6d1a9915eb3bbd660083532f7f8f7b11b
atfotccraig <49903079+atfotccraig@users.noreply.github.com>
2019-04-26 11:44:49 +0200

    Update offering.js

--- source/tracks/pleasing-you/offering.js
+++ source/tracks/pleasing-you/offering.js
@@ -27,32 +27,32 @@ const Offering = props => (
             <Line>Or surely I would bring</Line>
         </Verse>
         <Verse>
-            <Line>But the sacrifice that you desire</Line>
+            <Line>But the sacrifice that You desire</Line>
             <Line>is a broken, contrite heart</Line>
         </Verse>
         <Verse>
             <Line>I could never bring an offering</Line>
             <Line>when it hasn't cost me anything</Line>
             <Line>You paid the price for me</Line>
-            <Line>I'll count the cost for you</Line>
+            <Line>I'll count the cost for You</Line>
         </Verse>
         <Verse>
             <Line>Create in me a steadfast heart</Line>
             <Line>and cast me not away</Line>
-            <Line>Restore to me the joy you give</Line>
-            <Line>to those upheld by you</Line>
+            <Line>Restore to me the joy You give</Line>
+            <Line>to those upheld by You</Line>
         </Verse>
         <Verse repeat>
             <Line>Let my tongue sing aloud</Line>
-            <Line>of your righteousness</Line>
+            <Line>of Your righteousness</Line>
             <Line>Open up my lips</Line>
-            <Line>to declare your praise</Line>
+            <Line>to declare Your praise</Line>
         </Verse>
         <Verse>
             <Line>You desire truth</Line>
             <Line>in the inward parts</Line>
             <Line>and in the hidden place</Line>
-            <Line>you make me wise</Line>
+            <Line>You make me wise</Line>
         </Verse>
         <Verse>
             <Line>So wash me and I'll be clean</Line>
