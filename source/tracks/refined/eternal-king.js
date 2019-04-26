cdfb97f4edbf1565f55f531e08914e03a9b04736
atfotccraig <49903079+atfotccraig@users.noreply.github.com>
2019-04-26 10:41:59 +0200

    Update eternal-king.js

--- source/tracks/refined/eternal-king.js
+++ source/tracks/refined/eternal-king.js
@@ -36,28 +36,28 @@ const EternalKing = props => (
                 <Yahshua />, Messiah
             </Line>
             <Line>who is, and was, and is to come</Line>
-            <Line>Blessed be your name, Eternal King</Line>
+            <Line>Blessed be Your name, Eternal King</Line>
         </Verse>
         <Verse name="two">
             <Line>Beginning, and the end</Line>
-            <Line>The first, the last, the living one</Line>
-            <Line>Blessed be your name, Eternal King</Line>
+            <Line>The First, the Last, the Living One</Line>
+            <Line>Blessed be Your name, Eternal King</Line>
         </Verse>
         <Verse name="three">
             <Line>Coming on the clouds</Line>
             <Line>with the hosts of heaven</Line>
-            <Line>Set-apart are you!</Line>
+            <Line>Set-apart are You!</Line>
             <Line>Wonder, Counsellor, Everlasting Father</Line>
             <Line>
-                <Immanuel /> our king
+                <Immanuel /> our King
             </Line>
         </Verse>
         <Verse>
             <Line>Unchanging, everlasting</Line>
             <Line>perfector of all things</Line>
-            <Line>Eternal life is in you</Line>
-            <Line>I bow before your throne</Line>
-            <Line>and worship you alone</Line>
+            <Line>Eternal life is in You</Line>
+            <Line>I bow before Your throne</Line>
+            <Line>and worship You alone</Line>
         </Verse>
         <Repeat name="one" />
         <Repeat name="two" />
@@ -65,9 +65,9 @@ const EternalKing = props => (
         <Verse repeat>
             <Line>Unchanging, everlasting</Line>
             <Line>perfector of all things</Line>
-            <Line>Eternal life is in you</Line>
-            <Line>I bow before your throne</Line>
-            <Line>and worship you alone</Line>
+            <Line>Eternal life is in You</Line>
+            <Line>I bow before Your throne</Line>
+            <Line>and worship You alone</Line>
         </Verse>
     </Track>
 )
