adfc7fa03edc7a040ed5e40e425669a4fdcf7695
atfotccraig <49903079+atfotccraig@users.noreply.github.com>
2019-04-26 09:59:34 +0200

    Update all-things-new.js

--- source/tracks/because-of-the-lamb/all-things-new.js
+++ source/tracks/because-of-the-lamb/all-things-new.js
@@ -22,56 +22,56 @@ const AllThingsNew = props => (
             <Labels.BecauseOfTheLamb.AllThingsNew />
         </Title>
         <Verse>
-            <Line>You, you take my suffering</Line>
+            <Line>You, You take my suffering</Line>
             <Line>Turn it into blessing</Line>
-            <Line>You, you take my burdens</Line>
+            <Line>You, You take my burdens</Line>
             <Line>and set my heart free</Line>
         </Verse>
         <Verse>
-            <Line>You, you take my old life</Line>
+            <Line>You, You take my old life</Line>
             <Line>Exchange it for a new one</Line>
-            <Line>How can I thank you</Line>
-            <Line>for all that you’ve done?</Line>
+            <Line>How can I thank You</Line>
+            <Line>for all that You’ve done?</Line>
         </Verse>
         <Verse>
             <Line>You take my fear</Line>
-            <Line>and you give me new boldness</Line>
+            <Line>and You give me new boldness</Line>
             <Line>You take my strife</Line>
-            <Line>and in you I find rest</Line>
+            <Line>and in You I find rest</Line>
         </Verse>
         <Verse>
             <Line>You took my place</Line>
-            <Line>on that day when you died</Line>
+            <Line>on that day when You died</Line>
             <Line>Conquered my sin</Line>
-            <Line>there you made me alive</Line>
+            <Line>there You made me alive</Line>
         </Verse>
         <Verse repeat name="one">
             <Line>You took it all</Line>
             <Line>all I was, all I’ve done</Line>
-            <Line>You gave me yourself</Line>
-            <Line>and the freedom you’ve won</Line>
+            <Line>You gave me Yourself</Line>
+            <Line>and the freedom You’ve won</Line>
         </Verse>
         <Verse>
-            <Line>So let my life declare your splendour</Line>
-            <Line>Let my lips sing praise to you</Line>
+            <Line>So let my life declare Your splendour</Line>
+            <Line>Let my lips sing praise to You</Line>
             <Line>Let my hands be raised in worship</Line>
             <Line>
-                <Yahweh /> you make all things new, yeah
+                <Yahweh /> You make all things new, yeah
             </Line>
             <Line>
-                <Yahweh /> you make all things new, yeah
+                <Yahweh /> You make all things new, yeah
             </Line>
         </Verse>
         <Repeat name="one" />
         <Verse>
-            <Line>So let my life declare your splendour</Line>
-            <Line>Let my lips sing praise to you</Line>
+            <Line>So let my life declare Your splendour</Line>
+            <Line>Let my lips sing praise to You</Line>
             <Line>Let my hands be raised in worship</Line>
             <Line>
-                <Yahweh /> you make all things new, yeah
+                <Yahweh /> You make all things new, yeah
             </Line>
             <Line>
-                <Yahweh /> you make all things new
+                <Yahweh /> You make all things new
             </Line>
         </Verse>
     </Track>
