1997d5b2f8bc833cbf2c1e7af96ff0e448e50fa3
atfotccraig <49903079+atfotccraig@users.noreply.github.com>
2019-04-26 10:34:59 +0200

    Update tehillah-138.js

--- source/tracks/peace-with-you/tehillah-138.js
+++ source/tracks/peace-with-you/tehillah-138.js
@@ -22,16 +22,16 @@ const Tehillah138 = props => (
             <Labels.PeaceWithYou.Tehillah138 />
         </Title>
         <Verse>
-            <Line>I give you thanks with all my heart</Line>
+            <Line>I give You thanks with all my heart</Line>
             <Line>before the mighty ones</Line>
-            <Line>I bow myself before your throne</Line>
-            <Line>and sing my praise to you</Line>
+            <Line>I bow myself before Your throne</Line>
+            <Line>and sing my praise to You</Line>
         </Verse>
         <Verse>
-            <Line>The day I called you answered me</Line>
+            <Line>The day I called You answered me</Line>
             <Line>You made me bold and strengthened me</Line>
             <Line>Let all the rulers of the earth</Line>
-            <Line>give thanks for the words of your mouth</Line>
+            <Line>give thanks for the words of Your mouth</Line>
         </Verse>
         <Verse repeat>
             <Line>
@@ -43,11 +43,11 @@ const Tehillah138 = props => (
         </Verse>
         <Verse name="one">
             <Line>
-                Master <Yahweh />, you are perfect
+                Master <Yahweh />, You are perfect
             </Line>
-            <Line>In the midst of distress you revive me</Line>
-            <Line>You stretched out your hand to save me</Line>
-            <Line>and in kindness you never forsake me</Line>
+            <Line>In the midst of distress You revive me</Line>
+            <Line>You stretched out Your hand to save me</Line>
+            <Line>and in kindness You never forsake me</Line>
         </Verse>
         <Verse repeat>
             <Line>
