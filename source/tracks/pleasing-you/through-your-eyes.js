191887bdfcd9f5d9f40c897ffce8fc80c7649eb1
atfotccraig <49903079+atfotccraig@users.noreply.github.com>
2019-04-26 11:55:46 +0200

    Update through-your-eyes.js

--- source/tracks/pleasing-you/through-your-eyes.js
+++ source/tracks/pleasing-you/through-your-eyes.js
@@ -24,39 +24,39 @@ const ThroughYourEyes = props => (
             <Line>When the world is stripped away</Line>
             <Line>and all that’s left is me</Line>
             <Line>and I see myself</Line>
-            <Line>through your eyes</Line>
+            <Line>through Your eyes</Line>
         </Verse>
         <Verse>
             <Line>Then I wonder how it is</Line>
             <Line>that I came to be</Line>
-            <Line>so complete in your eyes</Line>
+            <Line>so complete in Your eyes</Line>
         </Verse>
         <Verse>
-            <Line>Because you saw me in my blood</Line>
+            <Line>Because You saw me in my blood</Line>
             <Line>and in my blood said “Live”</Line>
-            <Line>And then you washed me in your flood</Line>
-            <Line>I realised you forgive</Line>
+            <Line>And then You washed me in Your flood</Line>
+            <Line>I realised You forgive</Line>
         </Verse>
         <Verse>
-            <Line>Now I know you see the blood</Line>
-            <Line>that you spilled for me</Line>
-            <Line>when you look in my eyes</Line>
+            <Line>Now I know You see the blood</Line>
+            <Line>that You spilled for me</Line>
+            <Line>when You look in my eyes</Line>
         </Verse>
         <Verse>
-            <Line>Will you open up my eyes</Line>
+            <Line>Will You open up my eyes</Line>
             <Line>that I always see</Line>
-            <Line>what You see through your eyes?</Line>
+            <Line>what You see through Your eyes?</Line>
         </Verse>
         <Verse repeat>
-            <Line>Because you saw me in my blood</Line>
+            <Line>Because You saw me in my blood</Line>
             <Line>and in my blood said “Live”</Line>
-            <Line>And then you washed me in your flood</Line>
-            <Line>I realised you forgive</Line>
+            <Line>And then You washed me in your flood</Line>
+            <Line>I realised You forgive</Line>
         </Verse>
         <Verse>
             <Line>Let me live now everyday</Line>
             <Line>in the sacrifice</Line>
-            <Line>that makes me whole in your eyes</Line>
+            <Line>that makes me whole in Your eyes</Line>
         </Verse>
     </Track>
 )
