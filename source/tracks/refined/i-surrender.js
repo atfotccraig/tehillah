d611d7984575e2da854145c9bbb7fdd9ac959b05
atfotccraig <49903079+atfotccraig@users.noreply.github.com>
2019-04-26 10:47:47 +0200

    Update i-surrender.js

--- source/tracks/refined/i-surrender.js
+++ source/tracks/refined/i-surrender.js
@@ -24,40 +24,40 @@ const ISurrender = props => (
         </Title>
         <Verse>
             <Line>My fight is gone, I’m all alone</Line>
-            <Line>Only you know where I’ve been</Line>
+            <Line>Only You know where I’ve been</Line>
             <Line>I’m on the ground, there’s no way out</Line>
             <Line>Can’t beat this unseen foe</Line>
         </Verse>
         <Verse name="one">
             <Line>I surrender, I surrender</Line>
-            <Line>I give it all to you</Line>
+            <Line>I give it all to You</Line>
             <Line>I surrender, I surrender</Line>
-            <Line>my life belongs to you</Line>
+            <Line>my life belongs to You</Line>
         </Verse>
         <Verse>
-            <Line>The battle’s been long, but you are strong</Line>
+            <Line>The battle’s been long, but You are strong</Line>
             <Line>I can’t do this on my own</Line>
-            <Line>I’ll bend my knees, you set me free</Line>
+            <Line>I’ll bend my knees, You set me free</Line>
             <Line>Defeat all my enemies</Line>
         </Verse>
         <Repeat name="one" />
         <Verse name="two">
             <Line>Oh, I have hope</Line>
-            <Line>you’ve never let me down</Line>
+            <Line>You’ve never let me down</Line>
             <Line>You are my only salvation</Line>
         </Verse>
         <Verse name="three">
             <Line>When my friends let me down</Line>
-            <Line>you are always around</Line>
+            <Line>You are always around</Line>
             <Line>You are faithful beyond all measure</Line>
         </Verse>
         <Repeat name="two" />
         <Repeat name="three" />
         <Verse repeat repeatText="x3">
             <Line>I surrender, I surrender</Line>
-            <Line>I give it all to you</Line>
+            <Line>I give it all to You</Line>
             <Line>I surrender, I surrender</Line>
-            <Line>my life belongs to you</Line>
+            <Line>my life belongs to You</Line>
         </Verse>
     </Track>
 )
